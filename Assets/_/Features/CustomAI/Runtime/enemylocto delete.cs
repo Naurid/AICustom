2b9405d818349e7f00bb75542f329bf207da052a
Vilics Gabor <vilics@hotmail.com>
2023-07-07 14:01:38 +0200

    [real intialcommit]

---
+++ Assets/_/Features/CustomAI/Runtime/enemylocto delete.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyloctodelete : MonoBehaviour
+{
+    #region Public Members
+    #endregion
+
+
+    #region Unity API
+    #endregion
+
+
+    #region Private and Protected
+    #endregion
+}
